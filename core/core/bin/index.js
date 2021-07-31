bdae4169187d8b960cca9ac83b9898d4e05e4c30
henry.liu <liuhuan2472014@gmail.com>
2021-07-31 17:03:48 +0800

    项目准备

---
+++ core/core/bin/index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const utils = require('@lh-fe/utils')
+
+console.log("这是一个核心包");
+utils(1, 2);
---
+++ utils/utils/lib/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = utils;
+
+function utils(a, b) {
+    console.log("调用了utils工具函数");
+    return a + b
+}
