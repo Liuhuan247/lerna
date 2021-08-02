c7df44ce4e8131ef2de0bbb96e5396580d2f3c1d
henry.liu <liuhuan2472014@gmail.com>
2021-08-02 22:28:45 +0800

    实现获取npm最新的版本

--- utils/get-npm-info/lib/index.js
+++ utils/get-npm-info/lib/index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const semver = require('semver');
 const urlJoin = require('url-join');
 
-module.exports = {getNpmInfo};
+module.exports = {getNpmInfo, getNpmVersions, getNpmSemverVersions};
 
 function getNpmInfo(pkgName, registry) {
     if (!pkgName) return null;
@@ -23,4 +23,26 @@ function getNpmInfo(pkgName, registry) {
 
 function getDefaultRegistry(isOriginal = false) {
     return isOriginal ? "https://registry.npm.org" : "https://registry.npm.taobao.org"
+}
+
+async function getNpmVersions(pkgName, registry) {
+    const data = await getNpmInfo(pkgName, registry);
+    if (data) {
+        return Object.keys(data.versions)
+    }
+    return []
+}
+
+
+function getSemverVersions(baseVersion, versions) {
+    return versions.filter(version => semver.satisfies(version, `^${baseVersion}`)).sort((a, b) => semver.gt(b, a));
+}
+
+async function getNpmSemverVersions(baseVersions, pkgName, registry) {
+    const versions = await getNpmVersions(pkgName, registry);
+    const newVersions = getSemverVersions(baseVersions, versions);
+    if (newVersions && newVersions.length > 0) {
+        return newVersions[0];
+    }
+    return null;
 }
